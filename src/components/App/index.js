7a493e5a0a83d77492aabe32ac72ce01e38f9709
Linda Zhou <lz28330@gmail.com>
2018-10-19 22:50:47 -0400

    restructured; moved items from app/index to containers/index

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,30 +1,12 @@
 //change index  so import w/o additional file name
 import React, { Component } from 'react';
 import Intro from '../Intro'; //child component //since index no need to say name
+import Series from '../../containers/Series';
 import './App.css';
 import 'whatwg-fetch';
 
 //custom component capitalized ex: <Mycomponent />
 class App extends Component {
-  state = {
-    series: []
-  }
-  //immediate load after component been rendered
-  componentDidMount() {
-    fetch('http://api.tvmaze.com/search/shows?q=Vikings')
-      .then(response => response.json())
-      .then(json => this.setState({ series: json }))
-    //  .then((response) => {console.log(response)})
-    
-    // const series = ["Vikings", "Game of Thrones"];
-    //
-    // setTimeout(() => {
-    //   this.setState({ series: series });
-    // }, 2000);
-  }
-  //state: mount arg | or just {series} and applies both w/ same names
-  //setState tells componts+its children need to be rerendered w updated stated
-
   render() {
     return (
       <div className="App">
@@ -32,7 +14,7 @@ class App extends Component {
           <h1 className="App-title">TV Series List</h1>
         </header>
         <Intro message = 'Here you can find all of your most loved series'/>
-        The length of series array - {this.state.series.length}
+        <Series />
       </div>
     );
   }
--- src/containers/Series/index.js
+++ src/containers/Series/index.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
 
 class Series extends Component {
+  state = {
+    series: []
+  }
+  //immediate load after component been rendered
+  componentDidMount() {
+    fetch('http://api.tvmaze.com/search/shows?q=Vikings')
+      .then(response => response.json())
+      .then(json => this.setState({ series: json }))
+    //  .then((response) => {console.log(response)})
+
+    // const series = ["Vikings", "Game of Thrones"];
+    //
+    // setTimeout(() => {
+    //   this.setState({ series: series });
+    // }, 2000);
+  }
+  //state: mount arg | or just {series} and applies both w/ same names
+  //setState tells componts+its children need to be rerendered w updated stated
+
   render() {
     return (
-      
+
+      <div>The length of series array - {this.state.series.length}</div>
     )
   }
 }
+
+export default Series;
