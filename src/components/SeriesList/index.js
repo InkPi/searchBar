a0c4231b77b2cfc3f96bc8af394fafb30bb16243
Linda Zhou <lz28330@gmail.com>
2018-10-20 16:14:05 -0400

    making smaller serieslist components

--- src/components/SeriesList/index.js
+++ src/components/SeriesList/index.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import './index.css';
 
+//{series} stick all ur properties to which watever want ur props obj
+//was <li key={series.show.id}> in SeriesList
+const SeriesListItem = ({ series }) => (
+  <li>
+    {series.show.name}
+  </li>
+)
+
 const SeriesList = (props) => {
   return (
     <div>
       <ul className="series-list">
         {props.list.map(series => (
-          <li>{series.show.name}</li>
+          <SeriesListItem series={series} key={series.show.id}/>
         ))}
       </ul>
     </div>
   )
 }
+//index as key instead of id for last resort
 
 export default SeriesList;
