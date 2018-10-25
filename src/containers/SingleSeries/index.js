70e2a112ff49f56f1c13c8bc092b5d4bc4f6d9cd
Linda Zhou <lz28330@gmail.com>
2018-10-24 23:53:02 -0400

    added more for singleseries container

--- src/containers/SingleSeries/index.js
+++ src/containers/SingleSeries/index.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
+import Loader from '../../components/loader';
 
 //console folder match>params>id
 class SingleSeries extends Component {
+  state = {
+    show: null
+  }
+
+  componentDidMount() {
+    const { id } = this.props.match.params;
+
+    fetch(`http://api.tvmaze.com/search/shows/${id}?embed=episodes`)
+      .then(response => response.json())
+      .then(json => this.setState({ show: json }));
+  }
+
   render() {
     //console.log(this.props);
+    //<p>...{this.props.match.params.id</p>
     return (
       <div>
-        <p>Single Series - the show id will be {this.props.match.params.id}</p>
+        <p>Single Series - the show id will be</p>
       </div>
     )
   }
