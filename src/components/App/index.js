8a4a5b920cf2708e4209ca29464e32d1e39eafc0
Linda Zhou <lz28330@gmail.com>
2018-10-28 15:01:12 -0400

    linked the logo header back to home

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import Main from '../../components/Main';//child component //since index no need to say name
 import './App.css';
 import 'whatwg-fetch';
+import { Link } from 'react-router-dom';
 
 //custom component capitalized ex: <Mycomponent />
 class App extends Component {
@@ -10,7 +11,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">TV Series List</h1>
+          <Link to={`/`}><h1 className="App-title">TV Series List</h1></Link>
         </header>
         <Main />
       </div>
